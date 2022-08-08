2ed81f3f3b302453590c86b028cdbd70e95bc652
Estevão Gama <100967971+estgama@users.noreply.github.com>
2022-08-08 17:54:21 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -8,12 +8,12 @@ var criaMosquitoTempo = 1500
 var nivel = window.location.search
 nivel = nivel.replace('?', '')
 
-if(nivel === 'normal'){
+if(nivel === 'facil'){
+    criaMosquitoTempo = 2000
+} else if(nivel === 'medio'){
     criaMosquitoTempo = 1500
 } else if(nivel === 'dificil'){
     criaMosquitoTempo = 1000
-} else if(nivel === 'chucknorris'){
-    criaMosquitoTempo = 750
 }
 
 function ajustaTamanhoPalcoJogo(){
